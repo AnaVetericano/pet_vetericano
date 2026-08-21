ab10340872e56fe7192a06b7fcf272e03a96a593
Jubelit zapata <gjubelit@gmail.com>
2026-08-20 20:29:27 -0500

    Feat: Logica de inicio de sesión conectada a firabase y creación de un usuario

--- app/src/main/java/com/example/petvetericano/bienvenida.kt
+++ app/src/main/java/com/example/petvetericano/bienvenida.kt
@@ -63,6 +63,8 @@ private fun configurarEventos() {
 
 
     binding.ivNavPerfil.setOnClickListener {
+        val intent = Intent(this, editar_perfil::class.java)
+        startActivity(intent)
         Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
     }
 }

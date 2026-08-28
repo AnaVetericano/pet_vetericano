a1a74c07dddc014d3b33f2c4f585097ba29a0481
AnaVetericano <anyta822@gmail.com>
2026-08-28 01:45:18 -0500

    Feat: actualizacion apartado de adopcion y voluntariado

--- app/src/main/java/com/example/petvetericano/adopcion.kt
+++ app/src/main/java/com/example/petvetericano/adopcion.kt
@@ -15,45 +15,55 @@ class Adopcion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 1. Inicializamos ViewBinding
         binding = ActivityAdopcionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 2. Lista de datos de prueba
+        // Sustituye tus URLs reales aquí:
         val listaAnimales = listOf(
             AnimalCompania(
-                idFicha = "#INC-2025-000123",
-                nombre = "Pepito",
-                raza = "Chancolang",
-                descripcion = "Tierna, juguetona, no le gusta convivir con más animales."
+                idFicha = "#INC-2026-000123",
+                nombre = "Nena",
+                raza = "Mestiza / Ancianita",
+                descripcion = "Es ciega y sordita. Perrita adulta ancianita, rescatada de un refugio al lado del río Cauca. Aún guarda la esperanza de encontrar un hogar.",
+                urlImagen = "https://res.cloudinary.com/le5sk8qf/image/upload/v1787888763/Nena.jpg",
+                urlYoutube = "https://www.youtube.com/shorts/1kObwSpAXSw"
             ),
             AnimalCompania(
-                idFicha = "#INC-2025-000124",
-                nombre = "Chita",
-                raza = "Labrador",
-                descripcion = "Le encanta el agua y es muy juguetona."
+                idFicha = "#INC-2026-000124",
+                nombre = "Crispeta",
+                raza = "Mestiza (Silla de ruedas)",
+                descripcion = "Edad: 7 años aprox. Sufrió un accidente de tránsito en la variante sur, lo que la dejó en silla de ruedas y perdió un ojo. Sigue esperando un hogar.",
+                urlImagen = "https://res.cloudinary.com/le5sk8qf/image/upload/v1787888476/Crispeta.jpg",
+                urlYoutube = "https://www.youtube.com/shorts/c0KFiiSaDg4"
             ),
             AnimalCompania(
-                idFicha = "#INC-2025-000125",
-                nombre = "Beto",
-                raza = "Criollo",
-                descripcion = "Muy tranquilo y obediente."
+                idFicha = "#INC-2026-000125",
+                nombre = "Coco",
+                raza = "Mestizo",
+                descripcion = "Perrito que poco a poco ha ido perdiendo la visión. Ha superado sus temores y se encuentra listo para recibir mucho amor en un hogar.",
+                urlImagen = "https://res.cloudinary.com/le5sk8qf/image/upload/f_auto,q_auto/Coco",
+                urlYoutube = "https://www.youtube.com/shorts/ffV1BlJq_Rw"
+            ),
+            AnimalCompania(
+                idFicha = "#INC-2026-000126",
+                nombre = "Vampira",
+                raza = "Mestiza / Ancianita",
+                descripcion = "Perrita muy adulta, rescatada del refugio cerca al río Cauca. Ya no cuenta con dientes, pero aún espera un hogar que le brinde amor.",
+                urlImagen = "https://res.cloudinary.com/le5sk8qf/image/upload/v1787888749/Vampira.jpg",
+                urlYoutube = "..."
             )
         )
 
-        // 3. Conectamos los datos con la vista
         binding.recyclerViewAnimales.layoutManager = LinearLayoutManager(this)
         binding.recyclerViewAnimales.adapter = AdaptadorAdopcion(listaAnimales) { animalSeleccionado ->
             mostrarDialogoAdopcion(animalSeleccionado)
         }
 
-        // 4. Acción para el botón de regresar
         binding.btnAtras.setOnClickListener {
             finish()
         }
     }
 
-    // Ventana emergente que redirige a WhatsApp al presionar "Contactar al CBA"
     private fun mostrarDialogoAdopcion(animal: AnimalCompania) {
         val mensaje = "Gracias por salvar con amor, su solicitud será atendida bajo el número ${animal.idFicha}."
 
@@ -62,12 +72,9 @@ class Adopcion : AppCompatActivity() {
             .setMessage(mensaje)
             .setCancelable(true)
             .setPositiveButton("Contactar al CBA") { dialog, _ ->
-                // Reemplaza los 000000000 por el número de teléfono real del CBA
                 val numeroWhatsApp = "573012489098"
-
                 val url = "https://api.whatsapp.com/send?phone=$numeroWhatsApp&text=Hola,%20quiero%20consultar%20la%20solicitud%20${animal.idFicha}"
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-
                 startActivity(intent)
                 dialog.dismiss()
             }

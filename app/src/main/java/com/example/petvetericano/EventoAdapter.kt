bd18a2a7c559df0f5e015ce73e305c446c68a1b5
Jubelit <gjubelit@gmail.com>
2026-09-02 21:06:56 -0500

    feat:Creación de popup de ver detalles sin diseño por ahora

--- app/src/main/java/com/example/petvetericano/EventoAdapter.kt
+++ app/src/main/java/com/example/petvetericano/EventoAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.petvetericano.databinding.ItemEventoBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EventoAdapter(
     private val listaEventos: List<Evento>
@@ -15,48 +16,38 @@ class EventoAdapter(
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun vincular(evento: Evento) {
-
             binding.imgEvento.setImageResource(evento.imagen)
-
             binding.txtTipo.text = evento.tipo
-
             binding.txtTitulo.text = evento.titulo
-
             binding.txtFecha.text = "📅 ${evento.fecha}"
-
             binding.txtHora.text = "🕐 ${evento.hora}"
-
             binding.txtLugar.text = "📍 ${evento.lugar}"
 
+            // Evento de clic corregido para la ventana emergente
             binding.btnDetalles.setOnClickListener {
-                // Más adelante abriremos los detalles del evento
+                MaterialAlertDialogBuilder(binding.root.context)
+                    .setTitle(evento.titulo)
+                    .setMessage("Tipo: ${evento.tipo}\nFecha: ${evento.fecha}\nHora: ${evento.hora}\nLugar: ${evento.lugar}")
+                    .setPositiveButton("Cerrar") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
             }
         }
     }
 
-    // Crear la tarjeta
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): EventoViewHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventoViewHolder {
         val binding = ItemEventoBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-
         return EventoViewHolder(binding)
     }
 
     // Mostrar los datos
-    override fun onBindViewHolder(
-        holder: EventoViewHolder,
-        position: Int
-    ) {
-
+    override fun onBindViewHolder(holder: EventoViewHolder, position: Int) {
         val evento = listaEventos[position]
-
         holder.vincular(evento)
     }
 

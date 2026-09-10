1388388dade752e995c710211020056cf4827234
juniorsmith21 <ordonezjun0@gmail.com>
2026-09-09 23:42:04 -0500

    cambios

--- app/src/main/java/com/example/petvetericano/inicio_sesion.kt
+++ app/src/main/java/com/example/petvetericano/inicio_sesion.kt
@@ -30,6 +30,8 @@ class inicio_sesion : AppCompatActivity() {
     }
 
 
+
+
     private fun iniciarSesion() {
 
         val email = binding.editTextText.text.toString().trim()
@@ -49,12 +51,16 @@ class inicio_sesion : AppCompatActivity() {
             return
         }
 
+
         // Crear objeto que se enviará al backend
         val loginRequest = LoginRequest(
             email = email,
             password = password
         )
-
+        binding.txtRegistrarse.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+        }
         // Consumir API
         lifecycleScope.launch {
 

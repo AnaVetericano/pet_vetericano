e5efe17c840bf757d2f6f0c2a4c78a9ae2bcf85d
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-09-09 23:24:32 -0500

    feat: modulo perfil y paso 1 de peticiones en Django y Retrofit JWT en Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,9 @@ dependencies {
     // Glide para carga de imágenes y miniaturas
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Retrofit y OkHttp para conexión a Django
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
